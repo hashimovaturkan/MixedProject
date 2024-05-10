2911f05f6f4c70bd7def80d3bf549880b18bb20c
Turkan Hashimova <turkan.hashimova@azintelecom.az>
2024-05-10 10:46:27 +0400

    fix(fix): init

---
+++ src/main/java/com/example/mixedproject/MixedProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.mixedproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MixedProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MixedProjectApplication.class, args);
+	}
+
+}
